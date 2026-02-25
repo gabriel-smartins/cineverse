366a2bf6e482736f45091fc9f568ba51c16d4704
Gabriel Martins <gab.smartinn@outlook.com>
2026-02-25 18:12:37 -0300

    feat: implement findById, save and delete methods in category service

--- src/main/java/br/com/cineverse/service/CategoryService.java
+++ src/main/java/br/com/cineverse/service/CategoryService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +16,17 @@ public class CategoryService {
 
     public List<Category> findAll() {
         return categoryRepository.findAll();
+    }
+
+    public Optional<Category> findById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    public Category saveCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
     }
 }
