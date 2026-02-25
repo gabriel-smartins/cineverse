360e87f954501fe399bea4d72af6951729a0bb60
Gabriel Martins <gab.smartinn@outlook.com>
2026-02-25 18:12:42 -0300

    feat: add endpoints to get by id, create and delete categories

--- src/main/java/br/com/cineverse/controller/CategoryController.java
+++ src/main/java/br/com/cineverse/controller/CategoryController.java
@@ -3,9 +3,7 @@ package br.com.cineverse.controller;
 import br.com.cineverse.entity.Category;
 import br.com.cineverse.service.CategoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,26 @@ public class CategoryController {
     @GetMapping
     public List<Category> getAllCategories() {
         return categoryService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Category getCategoryById(@PathVariable("id") Long categoryId) {
+        var category = categoryService.findById(categoryId);
+
+        if (category.isPresent()) {
+            return category.get();
+        }
+
+        return null;
+    }
+
+    @PostMapping
+    public Category saveCategory(@RequestBody Category category) {
+        return categoryService.saveCategory(category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategoryById(@PathVariable("id") Long categoryId) {
+        categoryService.deleteCategory(categoryId);
     }
 }
