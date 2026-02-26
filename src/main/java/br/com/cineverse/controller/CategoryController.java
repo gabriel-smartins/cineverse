35e0af2306770bb7369fe1a2fec1290e93397eb2
Gabriel Martins <gab.smartinn@outlook.com>
2026-02-26 15:13:35 -0300

    refactor: update category controller to use DTOs

--- src/main/java/br/com/cineverse/controller/CategoryController.java
+++ src/main/java/br/com/cineverse/controller/CategoryController.java
@@ -1,6 +1,9 @@
 package br.com.cineverse.controller;
 
+import br.com.cineverse.controller.dto.request.CategoryRequestDTO;
+import br.com.cineverse.controller.dto.response.CategoryResponseDTO;
 import br.com.cineverse.entity.Category;
+import br.com.cineverse.mapper.CategoryMapper;
 import br.com.cineverse.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,24 +18,29 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping
-    public List<Category> getAllCategories() {
-        return categoryService.findAll();
+    public List<CategoryResponseDTO> getAllCategories() {
+        List<Category> categories = categoryService.findAll();
+
+        return categories.stream()
+                .map(CategoryMapper::toCategoryResponseDTO)
+                .toList();
     }
 
     @GetMapping("/{id}")
-    public Category getCategoryById(@PathVariable("id") Long categoryId) {
+    public CategoryResponseDTO getCategoryById(@PathVariable("id") Long categoryId) {
         var category = categoryService.findById(categoryId);
 
         if (category.isPresent()) {
-            return category.get();
+            return CategoryMapper.toCategoryResponseDTO(category.get());
         }
-
         return null;
     }
 
     @PostMapping
-    public Category saveCategory(@RequestBody Category category) {
-        return categoryService.saveCategory(category);
+    public CategoryResponseDTO saveCategory(@RequestBody CategoryRequestDTO request) {
+        var category = CategoryMapper.toCategory(request);
+        var savedCategory = categoryService.saveCategory(category);
+        return CategoryMapper.toCategoryResponseDTO(savedCategory);
     }
 
     @DeleteMapping("/{id}")
