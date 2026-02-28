43869724aa458121286b5a3409201d5a710f63aa
Gabriel Martins <gab.smartinn@outlook.com>
2026-02-28 14:18:39 -0300

    feat: add endpoint for user registration

---
+++ src/main/java/br/com/cineverse/controller/AuthController.java
@@ -0,0 +1,28 @@
+package br.com.cineverse.controller;
+
+import br.com.cineverse.controller.dto.request.UserRequestDTO;
+import br.com.cineverse.controller.dto.response.UserResponseDTO;
+import br.com.cineverse.entity.User;
+import br.com.cineverse.mapper.UserMapper;
+import br.com.cineverse.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> register(@RequestBody UserRequestDTO request) {
+        var user = userService.save(UserMapper.toUser(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toUserResponseDTO(user));
+    }
+}
