0dd2c32f3799f67f8c1249cb63be63caca5650b9
Gabriel Martins <gab.smartinn@outlook.com>
2026-02-28 15:29:40 -0300

    feat: implement password encryption and login logic

--- src/main/java/br/com/cineverse/service/UserService.java
+++ src/main/java/br/com/cineverse/service/UserService.java
@@ -3,6 +3,7 @@ package br.com.cineverse.service;
 import br.com.cineverse.entity.User;
 import br.com.cineverse.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,8 +11,12 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User save(User user) {
-        return userRepository.save(user);
+        return userRepository.save(user
+                .toBuilder()
+                .password(passwordEncoder.encode(user.getPassword()))
+                .build());
     }
 }
