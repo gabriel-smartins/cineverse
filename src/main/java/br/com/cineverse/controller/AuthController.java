4f88b95d5b5ab2ab3354b60dd5752ad7ee34b2d2
Gabriel Martins <gab.smartinn@outlook.com>
2026-02-28 15:29:52 -0300

    feat: add login endpoint

--- src/main/java/br/com/cineverse/controller/AuthController.java
+++ src/main/java/br/com/cineverse/controller/AuthController.java
@@ -1,6 +1,9 @@
 package br.com.cineverse.controller;
 
+import br.com.cineverse.config.TokenService;
+import br.com.cineverse.controller.dto.request.LoginRequestDTO;
 import br.com.cineverse.controller.dto.request.UserRequestDTO;
+import br.com.cineverse.controller.dto.response.LoginResponseDTO;
 import br.com.cineverse.controller.dto.response.UserResponseDTO;
 import br.com.cineverse.entity.User;
 import br.com.cineverse.mapper.UserMapper;
@@ -8,6 +11,9 @@ import br.com.cineverse.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +25,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
 
     @PostMapping("/register")
     public ResponseEntity<UserResponseDTO> register(@RequestBody UserRequestDTO request) {
         var user = userService.save(UserMapper.toUser(request));
         return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toUserResponseDTO(user));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO request) {
+        UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(request.email(), request.password());
+        Authentication authenticate = authenticationManager.authenticate(userAndPass);
+
+        User user = (User) authenticate.getPrincipal();
+
+        var token = tokenService.generateToken(user);
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 }
