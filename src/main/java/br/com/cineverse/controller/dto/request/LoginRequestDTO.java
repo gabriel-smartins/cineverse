816d0a940a82fe9359a01d5337132ca2d9513ef3
Gabriel Martins <gab.smartinn@outlook.com>
2026-02-28 15:29:34 -0300

    feat: add login request and response DTOs

---
+++ src/main/java/br/com/cineverse/controller/dto/request/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package br.com.cineverse.controller.dto.request;
+
+public record LoginRequestDTO(String email, String password) {
+}
---
+++ src/main/java/br/com/cineverse/controller/dto/response/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package br.com.cineverse.controller.dto.response;
+
+public record LoginResponseDTO(String token) {
+}
