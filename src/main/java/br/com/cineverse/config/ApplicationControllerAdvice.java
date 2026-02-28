fc8d28f1e5b37b9f6c73ba81a86de70ba1fd830d
Gabriel Martins <gab.smartinn@outlook.com>
2026-02-28 16:01:45 -0300

    feat: handle validation exceptions gracefully in controller advice

--- src/main/java/br/com/cineverse/config/ApplicationControllerAdvice.java
+++ src/main/java/br/com/cineverse/config/ApplicationControllerAdvice.java
@@ -2,9 +2,14 @@ package br.com.cineverse.config;
 
 import br.com.cineverse.exception.UsernameOrPasswordInvalidException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -13,5 +18,17 @@ public class ApplicationControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleNotFoundException(UsernameOrPasswordInvalidException e) {
         return e.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        e.getBindingResult().getAllErrors().forEach((error) ->
+                errors.put(((FieldError) error).getField(), error.getDefaultMessage())
+        );
+        return errors;
     }
 }
